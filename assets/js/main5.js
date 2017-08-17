a3dcae865c186e08b9823f19134164aeb5cc8372
mily rivas <mrise.translations@gmail.com>
2017-08-17 11:02:35 -0300

    agregando funcionalidad a cargar foto

---
+++ assets/js/main5.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+	var localNombre = localStorage.getItem('guadarName'); 
+	$('#nombre').html(localNombre); 
+	var localCorreo = localStorage.getItem('guadarEmail'); 
+	$('#correo').html(localCorreo); 
+
+
+
+	$('#files').on('click', function(){
+
+            $('#files').change(function(){
+
+             var reader = new FileReader();
+
+                reader.onload = function(file){
+
+                    var fileContent = file.target.result;
+
+                     //crear img y guardar en localStorage
+                    var img = '<img width="50%" src="' + fileContent + '">';
+                    localStorage.setItem('foto', img);
+
+           
+                }
+
+                reader.readAsDataURL(this.files[0]);
+
+
+      });
+
+  });
+
+     var localFoto = localStorage.getItem('foto');
+                    $("#imagen").html(localFoto);
+});
