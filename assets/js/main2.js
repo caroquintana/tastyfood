cdfc50978f9cba2bdd14eb7f6341f5e55d82f826
cindy <cindialy@gmail.com>
2017-08-17 09:09:27 -0300

    validacion2

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -16,3 +16,49 @@ $(document).ready(function() {
     })
 });
 
+//Validación Nombre del usuario.
+function validarName(){
+	var nombre = $('#name').val();
+	if(nombre == ""){
+		$('#noName').html('*Debe ingresar un nombre');
+		$('#name').val("");
+		return false;
+	}else{
+        $('#noName').html("");
+        localStorage.setItem('guardarName', nombre);
+        return true;
+	}
+};
+//Validación  de correo.
+function validarCorreo(){
+   var email = $('#email').val();
+
+   if(email == ""){
+		$('#noEmail').html('*Debe ingresar un email');
+		$('#email').val("");
+		return false;
+	}else if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test(email))) {
+        $('#noEmail').html('*Debe ingresar un email valido');
+        $('#email').val("");
+        return false;
+    }else{
+        $('#noEmail').html("");
+
+        localStorage.setItem('guardarEmail', email);
+        return true;
+	}
+};
+
+//Validación de contraseña.
+function validarPass(){
+	var pass = $('#password').val();
+
+	if(pass == ""){
+		$('#noPassword').html('*Debe ingresar una contraseña');
+		$('#password').val("");
+		return false;
+	}else{
+        $('#noPassword').html("");
+        return true;
+	}
+};
