14d2ef5f209a84b1d2faf63ffd793a3d48d879f0
caroquintana <caro.quintana.n@gmail.com>
2017-08-17 03:17:43 -0300

    cambios index3 master

--- assets/js/main3.js
+++ assets/js/main3.js
@@ -14,14 +14,39 @@ $(document).ready(function() {
 	.done(function(data) {
 		console.log("success");
 
-		/*data.restaurants.forEach(function(el){
-			var name = el.restaurant.name;
-			var direccion = el.restaurant.location.locality;
+		data.restaurants.forEach(function(el){
+			var namerestaurant = el.restaurant.name;
+			var comuna = el.restaurant.location.locality;
 			var fotoplato = el.restaurant.featured_image;
-			console.log(fotoplato);
+			var tipococina = el.restaurant.cuisines;
+			var rating = el.restaurant.user_rating.aggregate_rating;
+			var costopromedio = el.restaurant.average_cost_for_two;
+			var direccion = el.restaurant.location.address;
+			//console.log(fotoplato);
 			//$('.llamada').append('<ul><li>' + name + " " + direccion +" " + '<img src="'+fotoplato  +'" class="perfilplato"></li></ul>');
-			$('.llamada').append('<img src="' + fotoplato +'" class="perfilplato"><div class="continfo"><p class="titulorestorant"><b>'+ name +'</b>'+ direccion +' <i class="fa fa-cutlery" aria-hidden="true"></i></p></div>');
-		});*/
+			$('.llamada').append('<div class="col s4 center"><div class="contrestorant"><img src="'+fotoplato+'" class="perfilplato"><ul class="inforest"><li><p class="titulorest"><b>'+ namerestaurant +'</b></p></li><li><p class="comunarest">' + comuna + '</p></li><li><i class="fa fa-cutlery" aria-hidden="true"></i></li></ul><div class="oculto"><p class="ocultodireccion">'+ direccion+'</p><p class="ocultoprecio">'+ costopromedio+'</p><p class="ocultorating">'+ rating+'</p></div></div></div></div>');
+
+			$('.contrestorant').on('click',function(){
+				$('.inforestorant').empty();
+				var titulo = "";
+				var direfavoritos = "";
+				var preciofavoritos ="";
+				var ratingfavoritos = "";
+				var titulo = $(this).find(".titulorest").text();
+				var direfavoritos = $(this).find(".ocultodireccion").text();
+				var preciofavoritos = $(this).find(".ocultoprecio").text();
+				console.log(preciofavoritos);
+				var ratingfavoritos = $(this).find(".ocultorating").text();
+				
+
+				$('.inforestorant').append('<div class="infocabecera"><ul class="listfavorito"><li class="favcabecera"><h5 class="infotitulo">'+ titulo +'</h5></li><li class="favcabecera"><i class="fa fa-heart" aria-hidden="true"></i></li></ul></div><p class="titinfo"> Address</p><p class="infomenor">'+ direfavoritos +'</p><p class="titinfo">Price</p><p class="infomenor">'+ "$ "+ preciofavoritos+'</p><p class="titinfo">Rating</p><p class="infomenor">'+ ratingfavoritos+'</p>');
+					////
+
+
+
+			});
+
+		});
 		
 
 	})
